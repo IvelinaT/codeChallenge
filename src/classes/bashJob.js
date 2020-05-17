b393244e52b8bb5bdf9ff9fdc19b270d7eff0259
IvelinaT <ivelina.shishkova@MacPros-MacBook-Pro.local>
2020-05-17 23:16:24 +0300

    added integration tests

--- src/classes/bashJob.js
+++ src/classes/bashJob.js
@@ -1,26 +1,35 @@
 'use-strict'
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 const Job = require('./job')
 
 module.exports = class BashJob extends Job {
-  exportTasksInFile (tasks, filename) {
+  exportTasksInFile (tasks) {
     const result = tasks.map((orderedTask) => orderedTask.command)
-    result.unshift('#!/usr/bin/env bash\n')
-    filename = path.resolve(path.dirname(require.main.filename), filename)
-    
-    var file = fs.createWriteStream(filename, { mode: 0o755 })
-    file.on('finish', function () {
-      console.log('file has been written')
-    })
-    file.on('error', function (err) {
-      throw new Error(`Error creating bash fiile: ${err}`)
-    })
-    result.forEach(function (v) {
-      file.write(v + '\n')
-    })
-    file.end()
+    // other option is to save commands in a bash file and return it's full path
+    // result.unshift('#!/usr/bin/env bash\n')
+    // filename = path.resolve(path.dirname(require.main.filename), filename)
 
-    return filename
+    // var file = fs.createWriteStream(filename, { mode: 0o755 })
+    // file.on('finish', function () {
+    //   console.log('file has been written')
+    // })
+    // file.on('error', function (err) {
+    //   throw new Error(`Error creating bash fiile: ${err}`)
+    // })
+    // result.forEach(function (v) {
+    //   file.write(v + '\n')
+    // })
+    // file.end()
+
+    // return filename
+
+    const commands = `
+    #!/usr/bin/env bash
+    ${result.map(v => {
+      return v
+    }).join('\n')}
+    `
+    return commands
   }
 }
